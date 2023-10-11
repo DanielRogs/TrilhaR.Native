79520dd7f6711cf989f3f434706bee06f62325d6
Daniel Rodrigues <danielrocha.dr142@gmail.com>
2023-10-11 00:53:54 -0300

    Corrigindo a navigation

--- TrilhaNodeNative/src/pages/Desafio1/index.js
+++ TrilhaNodeNative/src/pages/Desafio1/index.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useFonts } from '@expo-google-fonts'
+
 
 // import { Container } from './styles';
 
 const Desafio1 = () => {
   
+    let [fontsLoaded] = useFonts({
+        'Nome-da-Fonte': require('./caminho-para-o-arquivo-da-fonte.ttf'), // Para projetos React Native puros
+      });
+    
+      if (!fontsLoaded) {
+        return <Text>Carregando...</Text>;
+      }
+
     return (
+        <LinearGradient
+            colors={['#3BC90C', '#1F7300']}
+            style={{ flex: 1 }}
+        >
+
         <View>
             
         </View>
+            
+
+        </LinearGradient>
     );
 }
 
-export default Desafio1;
+const style = StyleSheet.create({
+})
 
+export default Desafio1;
