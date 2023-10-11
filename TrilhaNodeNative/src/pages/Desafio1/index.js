8c9cebe79b1ce78f6885886ae1ea9a7c6d1607df
Daniel Rodrigues <danielrocha.dr142@gmail.com>
2023-10-10 21:05:45 -0300

    Configurando o navigation da aplicacao

---
+++ TrilhaNodeNative/src/pages/Desafio1/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Desafio1 = () => {
+  
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+export default Desafio1;
+
